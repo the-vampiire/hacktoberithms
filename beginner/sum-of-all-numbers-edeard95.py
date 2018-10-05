2501180724b0ca4f1bb7ada3311595739eada000
Ed <ed@localhost.localdomain>
2018-10-05 15:38:56 +0100

    Add sum of all numbers solution by edeard95

---
+++ beginner/sum-of-all-numbers-edeard95.py
@@ -0,0 +1,10 @@
+def sum_all(A, B):
+	AS = sum(A)
+	BS = sum(B)
+	TS = A+B
+	
+	print('The sum of list 1 is '+str(AS))
+	print('The sum of list 2 is '+str(BS))
+	print('The sum of both lists is '+str(TS))
+	return
+
