f974894394e08f9b065b480d8a432719bf06072c
Ed <ed@localhost.localdomain>
2018-10-05 16:42:05 +0100

    earning sum, think this abides by the rules

---
+++ beginner/sum_earnings_edeard95.py
@@ -0,0 +1,12 @@
+def sum_earnings(input):
+	A = input.split(',')
+	B = []
+	for i in A:
+		B.append(float(i))
+		if sum(B) < 0:
+		B = []
+	C = sum(B)
+	earn = round(C, 2)
+	print(earn)
+	return
+sum_earnings(input('Add this month\'s earnings:'))
