0772734a520189403b0b3760fd17575a5a16543b
Edeard95 <43120519+Edeard95@users.noreply.github.com>
2018-10-13 15:52:00 +0100

    Update sum-of-all-numbers-edeard95.py

--- beginner/sum-of-all-numbers-edeard95.py
+++ beginner/sum-of-all-numbers-edeard95.py
@@ -1,10 +1,16 @@
-def sum_all(A, B):
-	AS = sum(A)
-	BS = sum(B)
-	TS = A+B
-	
-	print('The sum of list 1 is '+str(AS))
-	print('The sum of list 2 is '+str(BS))
-	print('The sum of both lists is '+str(TS))
-	return
+A = [10,13,7]
+B = [1,2,3,4,5,6]
 
+def sum_all(A, B):
+	ASum = sum(A)
+	#print(ASum)
+	BSum = sum(B)
+	#print(BSum)
+	TSum = ASum+BSum
+	#print(TSum)
+	
+	return ASum, BSum, TSum
+ASum, BSum, TSum = sum_all(A,B)
+print('The sum of list 1 is '+str(ASum))
+print('The sum of list 2 is '+str(BSum))
+print('The sum of both lists is '+str(TSum))
