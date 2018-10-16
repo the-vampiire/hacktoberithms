9159a0c79c4165e7ae538f882437f2312666cccd
David Sargento <david.sargento10@gmail.com>
2018-10-16 22:56:58 +0200

    remove intructions

--- intermediate/mutation_dsargento.py
+++ intermediate/mutation_dsargento.py
@@ -1,28 +1,3 @@
-# > Credit: [adapted from Free Code Camp](https://learn.freecodecamp.org/javascript-algorithms-and-data-structures/basic-algorithm-scripting/mutations/)
-# > # Challenge
-# >
-# > Write a function that satisfies the following rules:
-# >
-# >     * Return true if the string in the first element of the list contains all of the letters of the string in the second element of the list.
-# >
-# >
-# > ## examples
-# >
-# > `["hello", "Hello"]`
-# >
-# >     * should return true because all of the letters in the second string are present in the first, ignoring case.
-# >
-# >
-# > `["hello", "hey"]`
-# >
-# >     * should return false because the string "hello" does not contain a "y".
-# >
-# >
-# > `["Alien", "line"]`
-# >
-# >     * should return true because all of the letters in "line" are present in "Alien".
-
-
 def mutation(word_list):
     if all(i in word_list[0].lower() for i in word_list[1].lower()):
         return True
