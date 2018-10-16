1772987f79d53cc7cca3c874b3d499c3cbd317c9
Zelbot <ysiegmann@web.de>
2018-10-16 18:39:29 +0200

    Removed set to account for repeated letters

--- intermediate/mutations_Zelbot.py
+++ intermediate/mutations_Zelbot.py
@@ -1,6 +1,7 @@
 def mutations(string_list):
     # We assume that the string_list contains two strings
-    letters = set(list(string_list[1].lower()))
+    # letters = set(list(string_list[1].lower()))
+    letters = list(string_list[1].lower())
     if all(letter in string_list[0].lower() for letter in letters):
         return True
     return False
