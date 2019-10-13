5e89ae6087012e0fdf6f505f5d5f1341c28c9e76
sokom141 <sokom141@gmail.com>
2019-10-13 17:01:20 +0200

    setup

---
+++ python/beginner/Profile-Lookup_Sokom141.py
@@ -0,0 +1,34 @@
+contacts = [
+    {
+        "firstName": "Akira",
+        "lastName": "Laine",
+        "number": "0543236543",
+        "likes": ["Pizza", "Coding", "Brownie Points"]
+    },
+    {
+        "firstName": "Harry",
+        "lastName": "Potter",
+        "number": "0994372684",
+        "likes": ["Hogwarts", "Magic", "Hagrid"]
+    },
+    {
+        "firstName": "Sherlock",
+        "lastName": "Holmes",
+        "number": "0487345643",
+        "likes": ["Intriguing Cases", "Violin"]
+    },
+    {
+        "firstName": "Kristian",
+        "lastName": "Vos",
+        "number": "unknown",
+        "likes": ["JavaScript", "Gaming", "Foxes"]
+    }
+];
+
+
+def look_up_profile(name, field):
+  pass # fill in your solution here
+
+
+// Change these values to test your function
+look_up_profile("Akira", "likes")
